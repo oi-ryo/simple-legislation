e9ea80975bd4905dd6b6c1ff03b6cb8682cf817c
oi-ryo <16907977+oi-ryo@users.noreply.github.com>
2021-05-13 09:58:52 +0900

    Add axios to fetch API

--- src/components/Elements.js
+++ src/components/Elements.js
@@ -3,11 +3,22 @@ import axios from "axios";
 import Element from "./Element";
 import LawData from "../mock/lawData.json";
 
-const lawData = LawData.data;
 function Elements() {
-  const [data, setData] = useState(lawData);
-  const elements = lawData.map((data) => (
-    <Element text={data.text} key={data.text} />
+  const [data, setData] = useState({ data: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios("URL");
+
+      setData(result.data);
+    };
+
+    fetchData();
+  });
+
+  console.log(data);
+  const elements = data.map((item) => (
+    <Element text={item.title} key={item.id} />
   ));
 
   return elements;
